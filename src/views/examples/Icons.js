8e50ee37687b50d139e12534633d5d0cd457bdda
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-05-12 15:14:11 +0300

    Update contact page

--- src/views/examples/Icons.js
+++ src/views/examples/Icons.js
@@ -1,21 +1,4 @@
-import { useState } from "react";
-// react component that copies the given text inside your clipboard
-import { CopyToClipboard } from "react-copy-to-clipboard";
-// reactstrap components
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  Container,
-  Row,
-  Col,
-  UncontrolledTooltip
-} from "reactstrap";
-// core components
-import Header from "components/Headers/Header.js";
-
 const Icons = () => {
-  const [copiedText, setCopiedText] = useState();
   return (
     <>
      
